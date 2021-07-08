c6667efee65760b36f678559828cee1a6119b1e5
AleAkiraH <alexsandersf15@gmail.com>
2021-07-08 16:00:28 -0300

    TANTOFAZ

---
+++ src/views/Register.vue
@@ -0,0 +1,28 @@
+<template>
+	<div>
+		{{currentPage}} / {{pageCount}}
+		<pdf
+			src="https://americalatina.dint.fgv.br/sites/americalatina.dint.fgv.br/files/teste33.pdf"
+			@num-pages="pageCount = $event"
+			@page-loaded="currentPage = $event"
+		></pdf>
+	</div>
+</template>
+
+<script>
+
+import pdf from 'vue-pdf'
+
+export default {
+	components: {
+		pdf
+	},
+	data() {
+		return {
+			currentPage: 0,
+			pageCount: 0,
+		}
+	}
+}
+
+</script>
